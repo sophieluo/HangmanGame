abd07b17fba5283dcf4b8887868be5d0e8cf3ef3
sophieluo <sophie@groobusiness.com>
2019-12-04 23:02:43 -0500

    generate java docs. not sure what is happening

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"hangman","l":"Controller"},{"p":"hangman","l":"EvilHangman"},{"p":"hangman","l":"Hangman"},{"p":"hangman","l":"TraditionalHangman"}]
